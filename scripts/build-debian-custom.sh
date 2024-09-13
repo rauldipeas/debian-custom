da8afe86d06f47e9868125ca2bc16eae33374148
Raul Dipeas <rauldipeas@disroot.org>
2024-09-13 01:12:09 -0300

    ^build-debian-custom

--- scripts/build-debian-custom.sh
+++ scripts/build-debian-custom.sh
@@ -50,12 +50,14 @@ wget -qO config/hooks/normal/virtualbox-x11.hook.chroot https://github.com/rauld
 wget -qO config/hooks/normal/purge.hook.chroot https://github.com/rauldipeas/debian-custom/raw/main/scripts/purge.sh
 wget -qO config/package-lists/desktop.list.chroot https://github.com/rauldipeas/debian-custom/raw/main/scripts/desktop-packages.list
 cd config/packages.chroot
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/FreeTubeApp/FreeTube/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/FreeTubeApp/FreeTube/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d'"' -f4)"
 dpkg-name freetube*.deb
 wget -q --show-progress https://prerelease.keybase.io/keybase_amd64.deb
 dpkg-name keybase*.deb
-wget -q --show-progress -O windscribe_amd64.deb https://prt.windscribe.com/install/desktop/linux_deb_x64
-dpkg-name windscribe*.deb
+wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/"$(wget -qO- https://repo.protonvpn.com/debian/dists/stable/main/binary-all/|grep protonvpn-stable|tail -n1|cut -d'"' -f12)"
+dpkg-name protonvpn*.deb
+#wget -q --show-progress -O windscribe_amd64.deb https://prt.windscribe.com/install/desktop/linux_deb_x64
+#dpkg-name windscribe*.deb
 cd ../..
 
 #wget -q --show-progress -O dekuve.zip 'https://www.dropbox.com/scl/fi/erhpzghrhpfcubofnnjdm/dekuve.zip?rlkey=advz5obcky8gm2sekumc3n63v&dl=1'
