b94a20eae4084a9010c6e122bd8cc1db24108d84
Raul Dipeas <rauldipeas@disroot.org>
2024-09-14 22:12:02 -0300

    ^protonvpn

--- scripts/protonvpn.sh
+++ scripts/protonvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 wget -q --show-progress https://repo.protonvpn.com/debian/dists/stable/main/binary-all/"$(wget -qO- https://repo.protonvpn.com/debian/dists/stable/main/binary-all/|grep protonvpn-stable|tail -n1|cut -d'"' -f2)"
-sudo -A apt install ./protonvpn*.deb
+sudo -A apt install -y ./protonvpn*.deb
 sudo -A apt update
-sudo -A install -y protonvpn-gui
+sudo -A apt install -y protonvpn-gui
