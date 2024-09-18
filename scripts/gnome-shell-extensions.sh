a21bdb3ffa50e446bc4c211a9766de12db15968d
Raul Dipeas <rauldipeas@disroot.org>
2024-09-18 16:19:02 -0300

    ^gnome-shell-extensions ^purge

--- scripts/gnome-shell-extensions.sh
+++ scripts/gnome-shell-extensions.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 pipx install gnome-extensions-cli --system-site-packages
+#arcmenu
+"$HOME"/.local/bin/gext install arcmenu@arcmenu.com
 #blur my shell
 "$HOME"/.local/bin/gext install blur-my-shell@aunetx
+#forge
+"$HOME"/.local/bin/gext install forge@jmmaranan.com
 mkdir -p /etc/skel/.local/share/gnome-shell
 mv /root/.local/share/gnome-shell/extensions /etc/skel/.local/share/gnome-shell/
 rm -rf "$HOME"/.*
--- scripts/purge.sh
+++ scripts/purge.sh
@@ -2,4 +2,5 @@
 set -e
 sudo -A apt autoremove --purge -y\
     "*im-config*"\
+    pipewire-pulse\
     "*yelp*"
