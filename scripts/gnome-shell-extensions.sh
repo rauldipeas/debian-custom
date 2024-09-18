fcf39a858ff14b6fdae4d0a4b9d4a83f39d80fcb
Raul Dipeas <rauldipeas@disroot.org>
2024-09-18 04:58:42 -0300

    ^desktop-packages ^gnome-shell ^dconf

--- scripts/gnome-shell-extensions.sh
+++ scripts/gnome-shell-extensions.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
-# arc menu
-# PLACEHOLDER
 pipx install gnome-extensions-cli --system-site-packages
 #blur my shell
 "$HOME"/.local/bin/gext install blur-my-shell@aunetx
-#floating panel
-# PLACEHOLDER
+mkdir -p /etc/skel/.local/share/gnome-shell
+mv /root/.local/share/gnome-shell/extensions /etc/skel/.local/share/gnome-shell/
+rm -rf "$HOME"/.*
