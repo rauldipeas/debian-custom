e61a741ebd7670a9510af765f1101a78e382259a
Raul Dipeas <rauldipeas@disroot.org>
2024-09-18 22:05:35 -0300

    ^build-debian ^desktop-packages ^gnome-shell-extensions

--- scripts/gnome-shell-extensions.sh
+++ scripts/gnome-shell-extensions.sh
@@ -5,8 +5,12 @@ pipx install gnome-extensions-cli --system-site-packages
 "$HOME"/.local/bin/gext install arcmenu@arcmenu.com
 #blur my shell
 "$HOME"/.local/bin/gext install blur-my-shell@aunetx
+#emoji copy
+"$HOME"/.local/bin/gext install emoji-copy@felipeftn
 #forge
 "$HOME"/.local/bin/gext install forge@jmmaranan.com
+#pano
+"$HOME"/.local/bin/gext install pano@elhan.io
 mkdir -p /etc/skel/.local/share/gnome-shell
 mv /root/.local/share/gnome-shell/extensions /etc/skel/.local/share/gnome-shell/
 rm -rf "$HOME"/.*
