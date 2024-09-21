d13429670140357abce66562a93fefadb307b0e9
Raul Dipeas <rauldipeas@disroot.org>
2024-09-21 03:44:09 -0300

    ^zen-browser

--- scripts/zen-browser.sh
+++ scripts/zen-browser.sh
@@ -2,7 +2,7 @@
 set -e
 sudo apt install -y	libfuse2
 sudo mkdir -p /opt/Applications
-sudo wget -cq --show-progress -O /opt/Applications/zen-generic.AppImage "$(wget -qO- https://api.github.com/repos/zen-browser/desktop/releases|grep browser_download_url|grep generic.AppImage|head -n1|cut -d '"' -f4)"
+sudo wget -cq --show-progress -O /opt/Applications/zen-generic.AppImage "$(wget -qO- https://api.github.com/repos/zen-browser/desktop/releases|grep browser_download_url|grep AppImage|head -n1|cut -d '"' -f4)"
 sudo chmod +x -v  /opt/Applications/zen-generic.AppImage
 sudo ln -sv /opt/Applications/zen-generic.AppImage /usr/bin/zen
 sudo wget -q --show-progress -O /usr/share/applications/zen.desktop https://github.com/zen-browser/desktop/raw/main/AppDir/zen.desktop
