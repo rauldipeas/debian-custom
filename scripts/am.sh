ac7c58dab214c21673f58597a34e82d8fa31fde6
Raul Dipeas <rauldipeas@disroot.org>
2024-09-21 21:39:22 -0300

    ^am ^dconf

--- scripts/am.sh
+++ scripts/am.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-wget https://github.com/ivan-hc/AM/raw/main/INSTALL
-chmod +x ./INSTALL
-sudo ./INSTALL
-rm ./INSTALL
+sudo bash <(wget -qO- https://github.com/ivan-hc/AM/raw/main/INSTALL)
+sudo chmod 777 /opt/am
