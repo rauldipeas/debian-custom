0dde8b30c3ae028ac3ab552d8e53bd6e31fa3e44
Raul Dipeas <rauldipeas@disroot.org>
2024-09-25 13:26:55 -0300

    ^fluent-gtk

--- scripts/hooks/fluent-gtk.sh
+++ scripts/hooks/fluent-gtk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt install -y sassc
+sudo apt install -y git sassc
 git clone https://github.com/vinceliuice/Fluent-gtk-theme
 cd Fluent-gtk-theme
 sudo ./install.sh\
@@ -9,4 +9,4 @@ sudo ./install.sh\
     --tweaks blur
 cd ..
 rm -r Fluent-gtk-theme
-sudo apt autoremove --purge -y sassc
+sudo apt autoremove --purge -y git sassc
