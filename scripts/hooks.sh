05f7872d5ee1afebd335d3754602da8cfe120a41
Raul Dipeas <rauldipeas@disroot.org>
2024-09-25 13:14:37 -0300

    ^hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
-find scripts/hooks -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
-cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot
-mv am.sh.hook.chroot 00-am.sh.hook.chroot
-mv alsa-firmware.sh.hook.chroot zz-alsa-firmware.sh.hook.chroot
-mv clean-root.sh.hook.chroot zz-clean-root.sh.hook.chroot
-cd
-find scripts/hooks/am -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
-find scripts/hooks/apt -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
+cd scripts/hooks
+find . -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
+mv /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/am.sh.hook.chroot /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/00-am.sh.hook.chroot
+mv /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/alsa-firmware.sh.hook.chroot /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/zz-alsa-firmware.sh.hook.chroot
+mv /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/clean-root.sh.hook.chroot /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot/zz-clean-root.sh.hook.chroot
+cd am
+find . -type f -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
+cd ../apt
+find . -type f -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
+cd 
