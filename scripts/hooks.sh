8a2a3d171280f4e01c54b97fed24d9b5cce560ef
Raul Dipeas <rauldipeas@disroot.org>
2024-09-25 13:10:37 -0300

    ^hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 find scripts/hooks -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
-"$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv am.sh.hook.chroot 00-am.sh.hook.chroot)"
-"$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv alsa-firmware.sh.hook.chroot zz-alsa-firmware.sh.hook.chroot)"
-"$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv clean-root.sh.hook.chroot zz-clean-root.sh.hook.chroot)"
+cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot
+mv am.sh.hook.chroot 00-am.sh.hook.chroot
+mv alsa-firmware.sh.hook.chroot zz-alsa-firmware.sh.hook.chroot
+mv clean-root.sh.hook.chroot zz-clean-root.sh.hook.chroot
+cd
 find scripts/hooks/am -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
 find scripts/hooks/apt -type f -maxdepth 1 -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
