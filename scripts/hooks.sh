13b4cc2051971604df52053b7a162553a133d20b
Raul Dipeas <rauldipeas@disroot.org>
2024-09-25 12:55:07 -0300

    ^hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-"$(cd scripts/hooks;find . -name "*.sh" -print0 -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;)"
+find scripts/hooks -name "*.sh" -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
 "$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv am.sh.hook.chroot 00-am.sh.hook.chroot)"
 "$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv alsa-firmware.sh.hook.chroot zz-alsa-firmware.sh.hook.chroot)"
 "$(cd /tmp/debian-custom/config/hooks/normal/am.sh.hook.chroot;mv clean-root.sh.hook.chroot zz-clean-root.sh.hook.chroot)"
-"$(cd scripts/hooks/am;find . -name "*.sh" -print0 -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;)"
-"$(cd scripts/hooks/apt;find . -name "*.sh" -print0 -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;)"
+#find scripts/hooks/am -name "*.sh" -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
+#find scripts/hooks/apt -name "*.sh" -printf "%f\n" -exec cp {} /tmp/debian-custom/config/hooks/normal/{}.hook.chroot \;
