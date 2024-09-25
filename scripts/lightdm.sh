fe73aa473eb7f170b32068f3381a6ba1ffcfc591
Raul Dipeas <rauldipeas@disroot.org>
2024-09-24 22:02:02 -0300

    +lightdm ^hooks ^purge

---
+++ scripts/lightdm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+sudo sed -i 's@#background=@background=/usr/share/backgrounds/gnome/blobs-d.svg@g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's@#icon-theme-name=@icon-theme-name=Papirus-Dark@g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's@#theme-name=@theme-name=Fluent-Dark-compact@g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's@#font-name=@font-name=Quicksand 11@g' /etc/lightdm/lightdm-gtk-greeter.conf
--- scripts/purge.sh
+++ scripts/purge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt autoremove --purge -y\
+    "gdm3*"\
     "*im-config*"\
     network-manager-gnome\
     pipewire-pulse\
