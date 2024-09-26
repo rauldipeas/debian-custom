860eadebcab11342dc0c174a73bc8f25aa64a9ea
Raul Dipeas <rauldipeas@disroot.org>
2024-09-26 02:40:40 -0300

    ^desktop-packages ^grub ^splash

--- scripts/hooks/grub.sh
+++ scripts/hooks/grub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
-sudo unzip debian-1080p.zip -d /usr/share/grub/themes/dark-matter
-rm debian-1080p.zip
+sudo unzip debian-1080p.zip -d /usr/share/grub/themes
+sudo rm debian-1080p.zip /usr/share/grub/themes/darkmatter-theme.py
 cat <<EOF |sudo tee /etc/default/grub.d/theme.cfg>/dev/null
-GRUB_THEME="/usr/share/grub/themes/dark-matter/theme.txt"
+GRUB_THEME="/usr/share/grub/themes/darkmatter/theme.txt"
 EOF
--- scripts/hooks/splash.sh
+++ scripts/hooks/splash.sh
@@ -2,11 +2,11 @@
 set -e
 find . -name "*splash*" -delete
 wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
-sudo unzip debian-1080p.zip -d boot/grub/dark-matter-theme
-rm debian-1080p.zip
-#sudo sed -i 's@/boot/grub/live-theme/theme.txt@/boot/grub/dark-matter-theme/theme.txt@g' boot/grub/theme.cfg
+sudo unzip debian-1080p.zip -d boot/grub/themes
+sudo rm debian-1080p.zip boot/grub/themes/darkmatter-theme.py
+#sudo sed -i 's@/boot/grub/live-theme/theme.txt@/boot/grub/thmes/darkmatter/theme.txt@g' boot/grub/theme.cfg
 cat <<EOF |sudo tee boot/grub/theme.cfg
 set color_normal=light-gray/black
 set color_highlight=white/dark-gray
-set theme=/boot/grub/dark-matter-theme/theme.txt
+set theme=/boot/grub/themes/darkmatter/theme.txt
 EOF
