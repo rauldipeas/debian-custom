b0f0353730214221428d5286e63dce06af0e4aca
Raul Dipeas <rauldipeas@disroot.org>
2024-09-26 03:19:28 -0300

    ^packages ^splash

--- scripts/hooks/apt/kernel.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-sudo apt autoremove --purge -y linux-image* linux-headers*
-sudo apt install -y -t bookworm-backports linux-headers-amd64 linux-image-amd64
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
-wget -qO /tmp/debian-custom/config/package-lists/desktop.list.chroot https://github.com/rauldipeas/debian-custom/raw/main/scripts/desktop-packages.list
+cp scripts/desktop-packages.list /tmp/debian-custom/config/package-lists/desktop.list.chroot
+#backports-kernel
+mkdir -p /tmp/debian-custom/config/archives
+cat <<EOF |tee config/archives/backports.list.chroot>/dev/null
+deb http://deb.debian.org/debian bookworm-backports main
+EOF
+cat <<EOF |tee config/package-lists/kernel.list>/dev/null
+linux-headers-$(dpkg --print-architecture) -t bookworm-backports
+linux-image-$(dpkg --print-architecture) -t bookworm-backports
+EOF
 cd /tmp/debian-custom/config/packages.chroot
 #atuin
 wget -cq --show-progress "$(wget -qO- https://api.github.com/repos/ellie/atuin/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
