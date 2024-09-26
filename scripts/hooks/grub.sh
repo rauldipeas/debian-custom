419417493b92e87a177b8411764b5f3518ad1853
Raul Dipeas <rauldipeas@disroot.org>
2024-09-26 02:33:54 -0300

    ^assets ^desktop-packages ^grub ^splash ^dconf

--- scripts/hooks/grub.sh
+++ scripts/hooks/grub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/Coopydood/HyperFluent-GRUB-Theme/releases|grep browser_download_url|grep debian|head -n1|cut -d '"' -f4)"
-sudo unzip hyperfluent-debian.tar.gz -d /usr/share/grub/themes/hyperfluent
-rm hyperfluent-debian.tar.gz
+wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
+sudo unzip debian-1080p.zip -d /usr/share/grub/themes/dark-matter
+rm debian-1080p.zip
 cat <<EOF |sudo tee /etc/default/grub.d/theme.cfg>/dev/null
-GRUB_THEME="/usr/share/grub/themes/hyperfluent/theme.txt"
+GRUB_THEME="/usr/share/grub/themes/dark-matter/theme.txt"
 EOF
--- scripts/hooks/splash.sh
+++ scripts/hooks/splash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 find . -name "*splash*" -delete
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/Coopydood/HyperFluent-GRUB-Theme/releases|grep browser_download_url|grep debian|head -n1|cut -d '"' -f4)"
-sudo unzip hyperfluent-debian.tar.gz -d boot/grub/hyperfluent-theme
-rm hyperfluent-debian.tar.gz
-#sudo sed -i 's@/boot/grub/live-theme/theme.txt@/boot/grub/hyperfluent-theme/theme.txt@g' boot/grub/theme.cfg
+wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
+sudo unzip debian-1080p.zip -d boot/grub/dark-matter-theme
+rm debian-1080p.zip
+#sudo sed -i 's@/boot/grub/live-theme/theme.txt@/boot/grub/dark-matter-theme/theme.txt@g' boot/grub/theme.cfg
 cat <<EOF |sudo tee boot/grub/theme.cfg
 set color_normal=light-gray/black
 set color_highlight=white/dark-gray
-set theme=/boot/grub/hyperfluent-theme/theme.txt
+set theme=/boot/grub/dark-matter-theme/theme.txt
 EOF
