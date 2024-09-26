556e2c68513b4c87346c06f88ac5985875d2764d
Raul Dipeas <rauldipeas@disroot.org>
2024-09-25 22:03:53 -0300

    ^assets ^desktop-packages ^packages ^plymouth

--- scripts/hooks/plymouth.sh
+++ scripts/hooks/plymouth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/debian-mac-style.zip
+wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-mac-style.zip
 unzip debian-mac-style.zip
 rm debian-mac-style.zip
 sudo cp -R debian-mac-style /usr/share/plymouth/themes/
