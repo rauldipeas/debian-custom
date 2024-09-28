731bbfa3f4bb8ef18505abf22aab713fd0b7758f
Raul Dipeas <rauldipeas@disroot.org>
2024-09-28 02:54:35 -0300

    ^lightdm ^nody-greeter

--- scripts/hooks/apt/nody-greeter.sh
+++ scripts/hooks/apt/nody-greeter.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/JezerM/nody-greeter/releases|grep browser_download_url|grep debian.deb|head -n1|cut -d '"' -f4)"
-sudo apt install -y build-essential git ./nody-greeter*.deb #npm
+sudo apt install -y ./nody-greeter*.deb
 sudo rm nody-greeter*.deb /usr/share/applications/nody-greeter.desktop
 sudo sed -i 's/greeter-session=lightdm-gtk-greeter/greeter-session=nody-greeter/g' /etc/lightdm/lightdm.conf
-#git clone https://github.com/hertg/lightdm-neon
-#cd lightdm-neon
-#make build
-#sudo make install
-#cd ..
-#rm -r lightdm-neon
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/hertg/lightdm-neon/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
 tar -xf build.tar.gz
+rm build.tar.gz
 sudo mv public /usr/share/web-greeter/themes/neon
 sudo sed -i 's/theme: gruvbox/theme: neon/g' /etc/lightdm/web-greeter.yml
-sudo apt autoremove --purge -y build-essential git #npm
--- scripts/hooks/lightdm.sh
+++ scripts/hooks/lightdm.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 set -e
 sudo sed -i 's@#background=@background=/usr/share/backgrounds/gnome/blobs-d.svg@g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's@#icon-theme-name=@icon-theme-name=Papirus-Dark@g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's@#theme-name=@theme-name=Fluent-Dark-compact@g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's@#font-name=@font-name=Quicksand 11@g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's@#indicators=@indicators=~spacer;~clock;~spacer;~session;~a11y;~power@g' /etc/lightdm/lightdm-gtk-greeter.conf
-#sudo rm /var/cache/lightdm/dmrc/*
-sudo mkdir -p /etc/lightdm/lightdm.conf.d
-cat <<EOF |sudo tee /etc/lightdm/lightdm.conf.d/seat.conf>/dev/null
-[Seat:*]
-user-session=gnome-xorg
-greeter-hide-users=false
-EOF
+sudo sed -i 's/#icon-theme-name=/icon-theme-name=Papirus-Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/#theme-name=/theme-name=Fluent-Dark-compact/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/#font-name=/font-name=Quicksand 11/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/#indicators=/indicators=~spacer;~clock;~spacer;~session;~a11y;~power/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/#greeter-hide-users/greeter-hide-users/g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/#user-session=default/user-session=gnome-xorg/g' /etc/lightdm/lightdm.conf
