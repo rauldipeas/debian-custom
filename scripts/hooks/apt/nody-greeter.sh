fc847739e2f34caf7f2239d4557d4764a4b1fcd7
Raul Dipeas <rauldipeas@disroot.org>
2024-09-28 00:48:42 -0300

    ^nody-greeter

--- scripts/hooks/apt/nody-greeter.sh
+++ scripts/hooks/apt/nody-greeter.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/JezerM/nody-greeter/releases|grep browser_download_url|grep debian.deb|head -n1|cut -d '"' -f4)"
-sudo apt install -y build-essential git ./nody-greeter*.deb npm
+sudo apt install -y build-essential git ./nody-greeter*.deb #npm
 sudo rm nody-greeter*.deb /usr/share/applications/nody-greeter.desktop
 sudo sed -i 's/greeter-session=lightdm-gtk-greeter/greeter-session=nody-greeter/g' /etc/lightdm/lightdm.conf
-git clone https://github.com/hertg/lightdm-neon
-cd lightdm-neon
-make build
-sudo make install
-cd ..
-rm -r lightdm-neon
+#git clone https://github.com/hertg/lightdm-neon
+#cd lightdm-neon
+#make build
+#sudo make install
+#cd ..
+#rm -r lightdm-neon
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/hertg/lightdm-neon/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
+tar -xf build.tar.gz
+sudo mv build/public /usr/share/web-greeter/themes/neon
 sudo sed -i 's/theme: gruvbox/theme: neon/g' /etc/lightdm/web-greeter.yml
-sudo apt autoremove --purge build-essential git npm
+sudo apt autoremove --purge -y build-essential git #npm
