92cf17834974e09962ec41b08a3625c25d2ce0bf
Raul Dipeas <rauldipeas@disroot.org>
2024-09-28 03:37:15 -0300

    ^apt ^nody-greeter

--- scripts/hooks/apt.sh
+++ scripts/hooks/apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cat <<EOF |sudo tee /etc/apt/apt.conf.d/100keep-edited-files
+cat <<EOF |sudo tee /etc/apt/apt.conf.d/100keep-edited-files>/dev/null
 Dpkg::Options {
    "--force-confdef";
    "--force-confold";
--- scripts/hooks/apt/nody-greeter.sh
+++ scripts/hooks/apt/nody-greeter.sh
@@ -3,7 +3,7 @@ set -e
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/JezerM/nody-greeter/releases|grep browser_download_url|grep debian.deb|head -n1|cut -d '"' -f4)"
 sudo apt install -y ./nody-greeter*.deb
 sudo rm nody-greeter*.deb /usr/share/applications/nody-greeter.desktop
-sudo sed -i 's/greeter-session=lightdm-gtk-greeter/greeter-session=nody-greeter/g' /etc/lightdm/lightdm.conf
+sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=nody-greeter/g' /etc/lightdm/lightdm.conf
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/hertg/lightdm-neon/releases|grep browser_download_url|grep tar.gz|head -n1|cut -d '"' -f4)"
 tar -xf build.tar.gz
 rm build.tar.gz
