bfde65558e5e439de4021e0704a06dc14a028ab2
Raul Dipeas <rauldipeas@disroot.org>
2024-09-30 23:09:38 -0300

    +papirus-icon-theme ^desktop-packages ^kernel ^packages

--- scripts/hooks/apt/kernel.sh
+++ scripts/hooks/apt/kernel.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
+cat <<EOF |sudo tee /etc/apt/sources.list.d/backports.list>/dev/null
+deb http://deb.debian.org/debian bookworm-backports main
+EOF
+cat <<EOF |tee /etc/apt/preferences.d/backports.pref>/dev/null
+Package: *
+Pin: release n=bookworm-backports
+Pin-Priority: -1
+EOF
 sudo apt autoremove --purge -y linux-image* linux-headers*
 sudo apt install -y -t bookworm-backports linux-image-amd64 linux-headers-amd64
--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -e
 cp scripts/desktop-packages.list /tmp/debian-custom/config/package-lists/desktop.list.chroot
-#backports-kernel
-#mkdir -p /tmp/debian-custom/config/archives
-#cat <<EOF |tee /tmp/debian-custom/config/archives/backports.list.chroot>/dev/null
-#deb http://deb.debian.org/debian bookworm-backports main
-#EOF
-#cat <<EOF |tee /tmp/debian-custom/config/archives/backports.pref.chroot>/dev/null
-#Package: *
-#Pin: release n=bookworm-backports
-#Pin-Priority: -1
-#EOF
-#cat <<EOF |tee /tmp/debian-custom/config/package-lists/kernel.list>/dev/null
-#linux-headers-$(dpkg --print-architecture) -t bookworm-backports
-#linux-image-$(dpkg --print-architecture) -t bookworm-backports
-#EOF
 cd /tmp/debian-custom/config/packages.chroot
 #atuin
 wget -cq --show-progress "$(wget -qO- https://api.github.com/repos/ellie/atuin/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
