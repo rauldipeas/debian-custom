af33c9ae59e3ace7ebb285052556ea8647522216
Raul Dipeas <rauldipeas@disroot.org>
2024-10-01 19:14:51 -0300

    ^splash

--- scripts/hooks/splash.sh
+++ scripts/hooks/splash.sh
@@ -4,11 +4,17 @@ find . -name "*splash*" -delete
 wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
 sudo unzip debian-1080p.zip -d boot/grub/themes
 sudo rm debian-1080p.zip boot/grub/themes/darkmatter-theme.py
+sudo mkdir -p boot/grub/fonts
+sudo cp boot/grub/themes/darkmatter/*.pf2 boot/grub/fonts/
 sudo sed -i 's/Live system/Debian custom/g' boot/grub/grub.cfg isolinux/live.cfg
 sudo sed -i 's/(amd64)//g' boot/grub/grub.cfg isolinux/live.cfg
 sudo sed -i 's/(amd64 /(/g' boot/grub/grub.cfg isolinux/live.cfg
 cat <<EOF |sudo tee boot/grub/theme.cfg
 set color_normal=light-gray/black
 set color_highlight=white/dark-gray
+loadfont hackb_18
+loadfont norwester_16
+loadfont norwester_20
+loadfont norwester_22
 set theme=/boot/grub/themes/darkmatter/theme.txt
 EOF
