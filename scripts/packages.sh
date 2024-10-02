a011976e2d88f24e2033381e8b349706a431926e
Raul Dipeas <rauldipeas@disroot.org>
2024-10-02 02:50:32 -0300

    ^desktop-packages ^packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -5,6 +5,8 @@ cd /tmp/debian-custom/config/packages.chroot
 #atuin
 wget -cq --show-progress "$(wget -qO- https://api.github.com/repos/ellie/atuin/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
 dpkg-name atuin*.deb
+wget -q --show-progress "$(wget -qO- https://www.veracrypt.fr/en/Downloads.html|grep amd64.deb|head -n1|cut -d '"' -f2|sed 's/&#43;/+/g')"
+dpkg-name veracrypt*.deb
 #sparkleshare
 #wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/deb/libappindicator3-0.1-cil_12.10.0+git20151221-5.1_amd64.deb
 #wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/deb/libwebkit2-sharp-4.0-cil_2.10.9+git20160917-1.1_amd64.deb
