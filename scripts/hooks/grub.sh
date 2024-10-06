51e6bb31649a824639f1e1bcd71e545b00f3ca99
Raul Dipeas <rauldipeas@disroot.org>
2024-10-06 12:46:38 -0300

    ^grub

--- scripts/hooks/grub.sh
+++ scripts/hooks/grub.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 wget -q --show-progress https://github.com/rauldipeas/debian-custom/raw/main/assets/compressed/debian-1080p.zip
-mkdir -p /usr/share/grub/themes
+sudo mkdir -p /usr/share/grub/themes
 sudo unzip debian-1080p.zip -d /usr/share/grub/themes
 sudo rm debian-1080p.zip /usr/share/grub/themes/darkmatter-theme.py
+sudo mkdir /etc/default/grub.d
 cat <<EOF |sudo tee /etc/default/grub.d/theme.cfg>/dev/null
 GRUB_THEME="/usr/share/grub/themes/darkmatter/theme.txt"
 GRUB_BACKGROUND=""
