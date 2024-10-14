5dd92636ce477c3bc660174eaa79706e1ffd541d
Raul Dipeas <rauldipeas@disroot.org>
2024-10-14 16:36:21 -0300

    ^gdm

--- scripts/hooks/apt/gdm.sh
+++ scripts/hooks/apt/gdm.sh
@@ -7,7 +7,7 @@ echo y|./install.sh
 sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/share/themes/Fluent-Dark/index.theme
 sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/share/themes/Fluent-Dark-compact/index.theme
 set-gdm-theme set Fluent-Dark-compact /usr/share/backgrounds/gnome/blobs-d.svg
-echo y|./uninstall --purge
+echo y|./uninstall.sh --purge
 cd ..
 rm -rf gdm-tools
 sudo apt autoremove --purge -y git
