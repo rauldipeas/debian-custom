bd776531d72531148cab05940fb227fef4b709a0
Raul Dipeas <rauldipeas@disroot.org>
2024-10-14 15:31:51 -0300

    ^gdm

--- scripts/hooks/apt/gdm.sh
+++ scripts/hooks/apt/gdm.sh
@@ -4,8 +4,8 @@ sudo apt install -y git
 git clone --depth=1 --single-branch https://github.com/realmazharhussain/gdm-tools
 cd gdm-tools
 echo y|./install.sh
-sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/themes/Fluent-Dark/index.theme
-sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/themes/Fluent-Dark-compact/index.theme
+sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/share/themes/Fluent-Dark/index.theme
+sudo sed -i 's/IconTheme=Fluent-Dark/Papirus-Dark/g' /usr/share/themes/Fluent-Dark-compact/index.theme
 set-gdm-theme set Fluent-Dark-compact /usr/share/backgrounds/gnome/blobs-d.svg
 echo y|./uninstall --purge
 cd ..
