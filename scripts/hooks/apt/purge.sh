3ab4dde3f8b24a745373069bce64e0f85ca83fcf
Raul Dipeas <rauldipeas@disroot.org>
2024-10-15 01:21:36 -0300

    ^dconf ^desktop-packages ^docs ^purge ^x11

--- scripts/hooks/apt/purge.sh
+++ scripts/hooks/apt/purge.sh
@@ -2,9 +2,12 @@
 set -e
 sudo apt autoremove --purge -y\
     "*apache*"\
+    gnome-accessibility-themes\
     gnome-icon-theme\
     gnome-remote-desktop\
     "*im-config*"\
+    libfishcamp1\
+    libsbig4\
     network-manager-gnome\
     pipewire-pulse\
     rygel\
--- scripts/hooks/x11.sh
+++ scripts/hooks/x11.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 sed -i 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm3/daemon.conf
-#sudo rm\
-#    /usr/share/xsessions/lightdm-xsession.desktop\
-#    /usr/share/wayland-sessions/gnome.desktop
-#sudo ln -s /dev/null /usr/share/xsessions/lightdm-xsession.desktop
-#sudo ln -s /dev/null /usr/share/wayland-sessions/gnome.desktop
 cat <<EOF |sudo tee /etc/X11/xorg.conf.d/00-touchpad.conf>/dev/null
 Section "InputClass"
     	Identifier "Touchpad"
