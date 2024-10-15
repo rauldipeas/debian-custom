066d8cbd01c29f7dd6a9bd576621f3c3699d6d4e
Raul Dipeas <rauldipeas@disroot.org>
2024-10-15 09:36:17 -0300

    ^packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -11,13 +11,13 @@ cd /tmp/debian-rdx/config/packages.chroot
 #atuin
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/ellie/atuin/releases|grep browser_download_url|grep .deb|head -n1|cut -d '"' -f4)"
 dpkg-name atuin*.deb
-wget -q --show-progress "$(wget -qO- https://api.github.com/repos/vagnum08/cpupower-gui/releases|grep browser_download_url|grep all.deb|head -n1|cut -d '"' -f4)"
-dpkg-name cpupower-gui*.deb
+#micro
 wget -q --show-progress "$(wget -qO- https://api.github.com/repos/zyedidia/micro/releases|grep browser_download_url|grep amd64.deb|head -n1|cut -d '"' -f4)"
 dpkg-name micro*.deb
+#veracrypt
 wget -q --show-progress "$(wget -qO- https://www.veracrypt.fr/en/Downloads.html|grep amd64.deb|head -n1|cut -d '"' -f2|sed 's/&#43;/+/g')"
 dpkg-name veracrypt*.deb
 #sparkleshare
-#wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libappindicator3-0.1-cil_12.10.0+git20151221-5.1_amd64.deb
-#wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libwebkit2-sharp-4.0-cil_2.10.9+git20160917-1.1_amd64.deb
-#wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/sparkleshare_3.28+git20190525+cf446c0-3_all.deb
+wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libappindicator3-0.1-cil_12.10.0+git20151221-5.1_amd64.deb
+wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libwebkit2-sharp-4.0-cil_2.10.9+git20160917-1.1_amd64.deb
+wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/sparkleshare_3.28+git20190525+cf446c0-3_all.deb
