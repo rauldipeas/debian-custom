e10cb612c3b29e9ff6f2957bc6fbce7610052ae0
Raul Dipeas <rauldipeas@disroot.org>
2024-10-15 19:41:06 -0300

    ^packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -21,3 +21,6 @@ dpkg-name veracrypt*.deb
 wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libappindicator3-0.1-cil_12.10.0+git20151221-5.1_amd64.deb
 wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/libwebkit2-sharp-4.0-cil_2.10.9+git20160917-1.1_amd64.deb
 wget -q --show-progress https://github.com/rauldipeas/debian-rdx/raw/main/assets/deb/sparkleshare_3.28+git20190525+cf446c0-3_all.deb
+#xournal++
+wget -q --show-progress "$(wget -qO- https://api.github.com/repos/xournalpp/xournalpp/releases|grep browser_download_url|grep -v dev|grep Debian|grep deb|head -n1|cut -d '"' -f4)"
+dpkg-name xournalpp*.deb
