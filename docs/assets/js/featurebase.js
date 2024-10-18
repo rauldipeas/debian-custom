85c3e0c8b40048c5d28558fb1e267876cdaa577e
Raul Dipeas <rauldipeas@disroot.org>
2024-10-17 23:45:23 -0300

    ^docs

---
+++ docs/assets/js/featurebase.js
@@ -0,0 +1,9 @@
+Featurebase("initialize_feedback_widget", {
+  organization: "debianrdx", // Replace this with your organization name, copy-paste the subdomain part from your Featurebase workspace url (e.g. https://*yourorg*.featurebase.app)
+  theme: "dark", // required
+  placement: "right", // optional - remove to hide the floating button
+  email: "featurebase.app.rauldipeas@lock.email", // optional
+  defaultBoard: "feedback", // optional - preselect a board
+  locale: "en", // Change the language, view all available languages from https://help.featurebase.app/en/articles/8879098-using-featurebase-in-my-language  
+  metadata: null // Attach session-specific metadata to feedback. Refer to the advanced section for the details: https://help.featurebase.app/en/articles/3774671-advanced#7k8iriyap66
+});
